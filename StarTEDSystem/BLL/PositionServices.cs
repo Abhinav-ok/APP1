0b94573ee45dad34b0d4b1f97ba1aa6119ce190e
Abhinav-ok <aaggarwal10@studentmail.nait.ca>
2025-12-16 12:23:05 -0700

    Implemented BLL query methods (Employees, Programs, Positions)

--- StarTEDSystem/BLL/PositionServices.cs
+++ StarTEDSystem/BLL/PositionServices.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StarTEDSystem.DAL;
+using StarTEDSystem.Entities;
+
 
 namespace StarTEDSystem.BLL
 {
-    internal class PositionServices
+    public class PositionServices
     {
+        private readonly StarTEDContext _context;
+
+        internal PositionServices(StarTEDContext registeredContext)
+        {
+            _context = registeredContext;
+        }
+        public List<Position> Position_GetList()
+        {
+            return _context.Positions.OrderBy(p => p.Description).ToList();
+        }
     }
 }
--- StarTEDSystem/BLL/ProgramServices.cs
+++ StarTEDSystem/BLL/ProgramServices.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StarTEDSystem.DAL;
+using StarTEDSystem.Entities;
 
 namespace StarTEDSystem.BLL
 {
-    internal class ProgramServices
+    public class ProgramServices
     {
+        private readonly StarTEDContext _context;
+
+        internal ProgramServices(StarTEDContext registeredContext)
+        {
+            _context = registeredContext;
+        }
+
+        public List<Entities.Program> Program_GetList()
+        {
+            return _context.Programs.OrderBy(p => p.ProgramName).ToList();
+        }
     }
 }
