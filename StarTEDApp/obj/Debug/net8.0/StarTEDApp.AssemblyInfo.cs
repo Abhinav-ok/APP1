d4b40bff6a6b6ac081325c5a9887cc2a9e4487f3
Abhinav-ok <aaggarwal10@studentmail.nait.ca>
2025-12-16 10:33:27 -0700

    Created the project structure and added the connection string

--- StarTEDApp/obj/Debug/net8.0/StarTEDApp.AssemblyInfo.cs
+++ StarTEDApp/obj/Debug/net8.0/StarTEDApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StarTEDApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5564c1811fd8f62b5be9d48d0c6a84be07ed25d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("StarTEDApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StarTEDApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
